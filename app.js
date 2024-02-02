33e16022feee1f379da85e4b01b8b6b352943ce7
Fred-Perso <127020205+Fred-Perso@users.noreply.github.com>
2024-02-02 22:17:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Remplacez 'YOUR_SPACE_ID' et 'YOUR_ACCESS_TOKEN' par les informations de votre espace Contentful.
-const spaceId = 'Blank';
-const accessToken = '4TWGfTm1LyHaY6qnApBE7m';
+const spaceId = '8pvw0ip6a9ui';
+const accessToken = '4dR9Ohm74VaxgQyCEc0JcBJij-0XjXcgAjBt-VzQuxU';
 const contentTypeId = 'DD';
 
 // URL de l'API Content Delivery
